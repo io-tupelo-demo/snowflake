5e71d1dce3141ec53c0e178cdc33cd455a7fe648
Alan <cloojure@gmail.com>
2021-09-23 22:43:42 -0700

    misc

--- test/clj/tst/demo/snowflake.clj
+++ test/clj/tst/demo/snowflake.clj
@@ -5,47 +5,53 @@
     [next.jdbc :as jdbc]
     ))
 
-(when false ; #todo get this working
+(import 'com.snowflake.client.jdbc.SnowflakeDriver)
+(Class/forName "net.snowflake.client.jdbc.SnowflakeDriver")
 
-  (import 'com.snowflake.client.jdbc.SnowflakeDriver)
-  (Class/forName "com.snowflake.client.jdbc.SnowflakeDriver")
+(def db-credentials nil
 
-  (def db-credentials nil
-
-  ; {:user         config/*snowflake-user*
-  ;  :dbname       "MF79778_EQUITY_DATA_SAMPLE_SHARE"
-  ;  :account-name config/*snowflake-account*
-  ;  :password     config/*snowflake-pass*
-  ;  }
-    )
-
-  (def snowflake-url (str
-                       "jdbc:snowflake://"
-                       (:account-name db-credentials)
-                       ".snowflakecomputing.com/"))
-
-  (def snowflake-conn
-    (let [props (doto (java.util.Properties.)
-                  (.put "account" (db-credentials :account-name))
-                  (.put "user" (db-credentials :user))
-                  (.put "password" (db-credentials :password))
-                  (.put "db" (db-credentials :dbname)))]
-      (java.sql.DriverManager/getConnection snowflake-url props)))
-
-  (def snowflake-ds (jdbc/get-datasource snowflake-url))
-
-  (dotest
-    (spyx-pretty
-      (jdbc/execute!
-        snowflake-conn
-        ["SELECT *
-          FROM PUBLIC.SAMPLE_EQUITY_VIEW
-         LIMIT 9"])))
-
+; {:user         config/*snowflake-user*
+;  :dbname       "MF79778_EQUITY_DATA_SAMPLE_SHARE"
+;  :account-name config/*snowflake-account*
+;  :password     config/*snowflake-pass*
+;  }
   )
 
-(dotest 
-  (spy :tst.demo.snowflake)
-  (is= 2 3)
+;(def snowflake-url (str
+;                     "jdbc:snowflake://"
+;                     (:account-name db-credentials)
+;                     ".snowflakecomputing.com/"))
+;(def snowflake-conn
+;  (let [props (doto (java.util.Properties.)
+;                (.put "account" (db-credentials :account-name))
+;                (.put "user" (db-credentials :user))
+;                (.put "password" (db-credentials :password))
+;                (.put "db" (db-credentials :dbname)))]
+;    (java.sql.DriverManager/getConnection snowflake-url props)))
 
+(def snowflake-url "jdbc:snowflake://RBA54017.snowflakecomputing.com" )
+
+(def snowflake-conn
+  (let [props (doto (java.util.Properties.)
+                (.put "user" "cloojure")
+                (.put "password" "#Swiper42")
+                (.put "warehouse" "COMPUTE_WH")
+                (.put "db" "SNOWFLAKE_SAMPLE_DATA")
+                (.put "schema" "WEATHER")
+                (.put "role" "ACCOUNTADMIN") ; or SYSADMIN
+                )]
+    (java.sql.DriverManager/getConnection snowflake-url props)))
+
+#_(def snowflake-ds (jdbc/get-datasource snowflake-url))
+
+;************************************************************************************************************
+;***** MUST USE JAVA 11 or get exception re:  https://github.com/snowflakedb/snowflake-jdbc/issues/484  *****
+;************************************************************************************************************
+(dotest
+  ; (spy :tst.demo.snowflake)
+  (let [raw (unlazy (jdbc/execute! snowflake-conn
+                       ["SELECT count(*) from SNOWFLAKE_SAMPLE_DATA.WEATHER.DAILY_14_TOTAL; "]))
+            ; raw => [{:COUNT(*) 36406252}]
+        count (-> raw only first val)]
+    (is (< count 300e6)))
 )
