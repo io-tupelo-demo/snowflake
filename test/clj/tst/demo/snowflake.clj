44911e6c090257210d9f649122bd66be5010e126
Alan <cloojure@gmail.com>
2021-10-11 13:31:20 -0700

    misc

--- test/clj/tst/demo/snowflake.clj
+++ test/clj/tst/demo/snowflake.clj
@@ -4,36 +4,12 @@
     [demo.config :as config]
     [next.jdbc :as jdbc]
     ))
-
-(spyx config/snowflake-user)
-(spyx config/snowflake-password)
-
-(import 'com.snowflake.client.jdbc.SnowflakeDriver)
 (Class/forName "net.snowflake.client.jdbc.SnowflakeDriver")
-
-(def db-credentials nil
-
-; {:user         config/*snowflake-user*
-;  :dbname       "MF79778_EQUITY_DATA_SAMPLE_SHARE"
-;  :account-name config/*snowflake-account*
-;  :password     config/*snowflake-pass*
-;  }
-  )
-
-;(def snowflake-url (str
-;                     "jdbc:snowflake://"
-;                     (:account-name db-credentials)
-;                     ".snowflakecomputing.com/"))
-;(def snowflake-conn
-;  (let [props (doto (java.util.Properties.)
-;                (.put "account" (db-credentials :account-name))
-;                (.put "user" (db-credentials :user))
-;                (.put "password" (db-credentials :password))
-;                (.put "db" (db-credentials :dbname)))]
-;    (java.sql.DriverManager/getConnection snowflake-url props)))
+  ; (import 'com.snowflake.client.jdbc.SnowflakeDriver)
 
 (def snowflake-url "jdbc:snowflake://RBA54017.snowflakecomputing.com" )
 
+#_(def snowflake-ds (jdbc/get-datasource snowflake-url))
 (def snowflake-conn
   (let [props (doto (java.util.Properties.)
                 (.put "user" config/snowflake-user)
@@ -44,8 +20,6 @@
                 (.put "role" "ACCOUNTADMIN") ; or SYSADMIN
                 )]
     (java.sql.DriverManager/getConnection snowflake-url props)))
-
-#_(def snowflake-ds (jdbc/get-datasource snowflake-url))
 
 ;************************************************************************************************************
 ;***** MUST USE JAVA 11 or get exception re:  https://github.com/snowflakedb/snowflake-jdbc/issues/484  *****
